95bf5cc40fc00d207560130a2aea030708c78ce0
jay091105 <23it004@charusat.edu.in>
2025-05-05 09:51:45 +0530

    user schema

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/employee-management')
+mongoose.connect('mongodb+srv://jay:jay212530@cluster0.f2cuqjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
